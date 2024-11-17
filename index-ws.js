0297c7b7a5bf5135db7e45c363d96f759099a1c1
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2024-11-17 11:52:38 +0200

    server

--- index-ws.js
+++ index-ws.js
@@ -1,11 +1,8 @@
 import express from "express";
-import { createServer } from 'http';
-import { WebSocketServer } from 'ws';
 import path from 'path';
 
 const app = express();
-const PORT = 3000;
-const server = createServer(app);
+const port = 3000;
 const __dirname = path.resolve();
 
 app.use(express.static(__dirname));
@@ -14,44 +11,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-server.on('request', app);
-
-server.listen(PORT, function () { console.log('Listening on ' + PORT); });
-
-/** Websocket **/
-
-const wss = new WebSocketServer({ server: server });
-
-wss.on('connection', function connection(ws) {
-  const numClients = wss.clients.size;
-
-  console.log('clients connected: ', numClients);
-
-  wss.broadcast(`Current visitors: ${numClients}`);
-
-  if (ws.readyState === ws.OPEN) {
-    ws.send('welcome!');
-  }
-
-  ws.on('close', function close() {
-    wss.broadcast(`Current visitors: ${wss.clients.size}`);
-    console.log('A client has disconnected');
-  });
-
-  ws.on('error', function error() {
-    //
-  });
+app.listen(port, (req, res) => {
+  console.log(`Server running on port ${port}.`);
 });
-
-/**
- * Broadcast data to all connected clients
- * @param  {Object} data
- * @void
- */
-wss.broadcast = function broadcast(data) {
-  console.log('Broadcasting: ', data);
-  wss.clients.forEach(function each(client) {
-    client.send(data);
-  });
-};
-/** End Websocket **/
